f3b2890d238ba7bcbf15c03be12c9c7ee4be1e98
mayalei <mayalei299@gmail.com>
2024-10-23 17:03:32 -0400

    Fixed Checkstyle error

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,6 @@
 package interface_adapter.logout;
 
-import use_case.login.LoginInputData;
+// import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
 import use_case.logout.LogoutInputData;
 
